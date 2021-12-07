0890734bfc8d28aed850ac722c63a4482f7dafc6
Marco Macaluso <macaluso@hotmail.co.uk>
2021-12-07 12:47:10 +0000

    added function to get article based on a sql query based on the ID of the article

--- includes/article.php
+++ includes/article.php
@@ -15,20 +15,27 @@ function getArticle($conn, $id)
             FROM ARTICLE
             WHERE id = ?";       
 
+    // turns sql query into prepared statement so as to avoid sql injection
     $preparedStmt = mysqli_prepare($conn, $sql);
 
+    // checks if there's an error and if there is, error is printed
     if ($preparedStmt === false) 
     {
         echo mysqli_error($conn);
     }
     else
     {
+        // binds parameter to the placeholder and passes in the $id argument and setting it as an int 
         mysqli_stmt_bind_param($preparedStmt, "i", $id);
 
+        //executes the sql statement which returns true if succesful 
         if (mysqli_stmt_execute($preparedStmt))
         {
+            // $result is the result of the sql statement 
            $result =  mysqli_stmt_get_result($preparedStmt);
 
+           // returns an associative array based on $result variable which 
+           // in turn is the result of the sql query
            return mysqli_fetch_array($result, MYSQLI_ASSOC);
         }
     }
