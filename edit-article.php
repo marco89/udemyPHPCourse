ac86beb8662744a1831081ae7d96003d03ce8597
Marco Macaluso <macaluso@hotmail.co.uk>
2021-12-08 16:44:16 +0000

    added code for validation check

--- edit-article.php
+++ edit-article.php
@@ -8,12 +8,28 @@ $conn = getDB();
 if (isset($_GET['id'])) {
 
     $article = getArticle($conn, $_GET['id']);
-    }
-    else
-    {
-        $article = null;
+
+    if ($article) {
+    // data coming from db is in assoc array so this assigns values to the expected variables
+    $title = $article['title'];
+    $content = $article['content'];
+    $published_at = $article['published_at'];
+
+    } else {
+        // exits the script if the if statement above returns false/null
+        die("Article not found");
     }
 
-var_dump($article);
+} else {
+    // this is displayed if the article ID isn't passed into the url
+    die("ID not supplied, article not found");
+}
 
 ?>
+<?php require 'includes/header.php'; ?>
+
+<h2>Edit article</h2>
+    
+<?php require 'includes/article-form.php'; ?>
+    
+<?php require 'includes/footer.php'; ?>
