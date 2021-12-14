9dc3aaaf6ec210043606ad69f8323bbc6af43a85
Marco Macaluso <macaluso@hotmail.co.uk>
2021-12-14 10:54:38 +0000

    added ability to delete article

---
+++ delete-article.php
@@ -0,0 +1,49 @@
+<?php
+
+require 'includes/database.php';
+require 'includes/article.php';
+require 'includes/url.php';
+
+$conn = getDB();
+
+if (isset($_GET['id'])) {
+
+    $article = getArticle($conn, $_GET['id']);
+
+    if ($article) {
+        // data coming from db is in assoc array so this assigns values to the expected variables
+        $id = $article['id'];
+        $title = $article['title'];
+        $content = $article['content'];
+        $published_at = $article['published_at'];
+    } else {
+        // exits the script if the if statement above returns false/null
+        die("Article not found");
+    }
+} else {
+    // this is displayed if the article ID isn't passed into the url
+    die("ID not supplied, article not found");
+}
+
+$sql = "DELETE FROM article
+        WHERE id = ?";
+
+// makes a prepared statement to stop injections
+$stmt = mysqli_prepare($conn, $sql);
+
+if ($stmt === false) {
+
+    echo mysqli_error($conn);
+} else {
+
+    //binds the last 3 args to use as placeholders in the sql variable above (s for string, i for integer etc)
+    mysqli_stmt_bind_param($stmt, "i", $id);
+
+    if (mysqli_stmt_execute($stmt)) {
+
+        redirect("/udemy/index.php");
+    } else {
+
+        echo mysqli_stmt_error($stmt);
+    }
+}
