24a08892e8d7d3a1a12c406d5d675d8135a8dcd2
Marco Macaluso <macaluso@hotmail.co.uk>
2021-12-15 15:45:24 +0000

    added code to delete session data

--- logout.php
+++ logout.php
@@ -1,5 +1,26 @@
 <?php
 
+require 'includes/url.php';
+
+// creates a session or resumes the current one based on a session identifier passed via a GET or POST request, or passed via a cookie. 
 session_start();
 
-$_SESSION['is_logged_in'] = false;
+// Unsets all the sessions variables by clearing the $_SESSION variable and setting it to an empty array
+$_SESSION = array();
+
+// Below deletes the session cookie, specifically it kills the session and not just the session data
+
+// checks whether the session if using cookies using an if statement
+if (ini_get("session.use_cookies")) {
+    // if it does use cookies, the following code deletes all the cookie data
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// destroys all the data from the session
+session_destroy();
+
+redirect('/udemy');
