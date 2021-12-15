22ba75928790f5706a8ae198edf5ab4331225d77
Marco Macaluso <macaluso@hotmail.co.uk>
2021-12-15 11:54:51 +0000

    added user login/password input

--- login.php
+++ login.php
@@ -1,9 +1,56 @@
 <?php
 
+// requires the file that contains the redirect function
+require 'includes/url.php';
+
 session_start();
 
-?>
+// checks whether POST method is used 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+    // checks user credentials
+    if ($_POST['username'] == 'cms_www' && $_POST['password'] == 'ooSgk2)yoWsYj*z8') {
+
+        $_SESSION['is_logged_in'] = true;
+
+        // if credentials are correct, redirects back to index page
+        redirect('/udemy');
+
+    } else {
+
+        // if credentials are wrong, pushes login error message to $error variable
+        $error = "login incorrect";
+
+    }
+}
+
+?>
 <?php require 'includes/header.php'; ?>
+
+<h2>Login</h2>
+
+<!-- if $error var isn't empty i.e. if credentials are wrong, print $error -->
+<?php if (! empty($error)) : ?>
+    <p><?= $error ?></p>
+<?php endif; ?>
+
+<!-- specifying the POST method ensures that login details dont appear in URL -->
+<form method="post">
+
+    <!-- adds username input and placeholder -->
+    <div>
+        <label for="username">Username</label>
+        <input name="username" id="username">
+    </div>
+
+    <!-- adds pw input and placeholder -->
+    <div>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+    </div>
+
+    <button>Log in</button>
+
+</form>
 
 <?php require 'includes/footer.php'; ?>
