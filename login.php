356c25d87ba288a48e42f91a1cc9711097e6035c
Marco Macaluso <macaluso@hotmail.co.uk>
2021-12-15 15:45:46 +0000

    added session_regenerate_id

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 // requires the file that contains the redirect function
 require 'includes/url.php';
 
+// creates a session or resumes the current one based on a session identifier passed via a GET or POST request, or passed via a cookie. 
 session_start();
 
 // checks whether POST method is used 
@@ -11,12 +12,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // checks user credentials
     if ($_POST['username'] == 'cms_www' && $_POST['password'] == 'ooSgk2)yoWsYj*z8') {
 
+        // creates a new session ID to prevent Session Fixation Attacks
+        // https://owasp.org/www-community/attacks/Session_fixation
+        session_regenerate_id(true);
+
         $_SESSION['is_logged_in'] = true;
 
         // if credentials are correct, redirects back to index page
         redirect('/udemy');
 
-    } else {
+    } 
+    else
+    {
 
         // if credentials are wrong, pushes login error message to $error variable
         $error = "login incorrect";
