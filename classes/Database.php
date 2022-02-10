f6d7827132b248da4a12b64b99eb2530c8627acd
Marco Macaluso <marco.macaluso@incuto.com>
2022-02-10 13:26:44 +0000

    Created a DB OOP file

---
+++ classes/Database.php
@@ -0,0 +1,18 @@
+<?php
+
+class Database
+{
+    public function getDB()
+    {
+        $db_host = "localhost";
+        $db_name = "cms";
+        $db_user = "cms_www";
+        $db_pass = "ooSgk2)yoWsYj*z8";
+
+        // specifies the type, host and name of db as well as what charset
+        $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
+
+        // creates a new PDO object 
+        $conn = new PDO($dsn, $db_user, $db_pass);
+    }
+}
