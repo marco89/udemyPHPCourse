3428d3bdcd8831a9e932495432b72e99d0f488eb
Marco Macaluso <marco.macaluso@incuto.com>
2022-02-11 10:45:46 +0000

    changed formatting

--- includes/database.php
+++ includes/database.php
@@ -6,19 +6,19 @@
  * @return object Connects to the mysql server
  */
 function getDB()
-{
-$db_host = "localhost";
-$db_name = "cms";
-$db_user = "cms_www";
-$db_pass = "ooSgk2)yoWsYj*z8";
+    {
+        $db_host = "localhost";
+        $db_name = "cms";
+        $db_user = "cms_www";
+        $db_pass = "ooSgk2)yoWsYj*z8";
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+        $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
-    exit;
-}
+        if (mysqli_connect_error()) {
+            echo mysqli_connect_error();
+            exit;
+    }
 
-return $conn;
+    return $conn;
 
 }
